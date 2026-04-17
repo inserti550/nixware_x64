19703aff75cefea18a5f67165fa669a2dfde1157
Nazar <inserti550@gmail.com>
2026-04-17 15:39:37 +0300

    correct delay

--- dllmain.cpp
+++ dllmain.cpp
@@ -24,7 +24,7 @@ DWORD WINAPI dll_entry(LPVOID thread_parameter)
     hooks::initialize();
 
     while (!globals::unload)
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     dll_detach();
 
